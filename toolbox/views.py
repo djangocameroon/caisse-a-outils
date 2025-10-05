aa0f030fca8b8138ce05fb0b9473b6f9c197270f
Tomdieu <ivantomdio@gmail.com>
2025-10-06 00:03:16 +0100

    refactor: replace data retrieval functions with ORM queries in views

--- toolbox/views.py
+++ toolbox/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 from .data import get_all_tools, get_tool
 from .resources import get_all_resources, get_resource
 
+from .models import Tool, Resource
+
 
 def home(request):
-    tools = get_all_tools()
-    resources = get_all_resources()
+    tools = Tool.objects.all().order_by("name")
+    resources = Resource.objects.all().order_by("title")
     context = {
         "hero_tools": tools[:4],
         "hero_resources": resources[:3],
@@ -16,26 +18,26 @@ def home(request):
 
 
 def tool_list(request):
-    tools = get_all_tools()
+    tools = Tool.objects.all().order_by("name")
     return render(request, "toolbox/tool_list.html", {"tools": tools})
 
 
 def tool_detail(request, slug: str):
     try:
-        tool = get_tool(slug)
-    except KeyError as exc:
+        tool = Tool.objects.get(slug=slug)
+    except Tool.DoesNotExist as exc:
         raise Http404("Outil introuvable") from exc
     return render(request, "toolbox/tool_detail.html", {"tool": tool})
 
 
 def resource_list(request):
-    resources = get_all_resources()
+    resources = Resource.objects.all().order_by("title")
     return render(request, "toolbox/resource_list.html", {"resources": resources})
 
 
 def resource_detail(request, slug: str):
     try:
-        resource = get_resource(slug)
-    except KeyError as exc:
+        resource = Resource.objects.get(slug=slug)
+    except Resource.DoesNotExist as exc:
         raise Http404("Ressource introuvable") from exc
     return render(request, "toolbox/resource_detail.html", {"resource": resource})
